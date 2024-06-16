205b2880da7fc7631d4aa4c3a397101b78368e47
WebCoderLider <webcoderlider@gmail.com>
2024-06-16 13:51:54 +0300

    teacher

--- src/Components/Teachers/Teachers.jsx
+++ src/Components/Teachers/Teachers.jsx
@@ -28,6 +28,19 @@ function Teachers() {
   return (
     <div className="container my-5 py-5">
       <h2 className="text-center mb-5">O'qituvchilar</h2>
+
+      <div className="d-flex justify-content-center">
+        <div className="col-lg-3 col-md-4 col-sm-6 mb-4">
+          <div className="card teacher-card shadow-sm">
+            <img src={'https://img.lovepik.com/photo/20230421/medium/lovepik-old-female-english-teacher-and-students-in-the-classroom-photo-image_352156615.jpg'} className="card-img-top rounded-circle mx-auto mt-3" />
+            <div className="card-body text-center">
+              <h5 className="card-title">Ra'no Ra'noeva</h5>
+              <p className="card-text">Direktorimz</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="row">
         {teachers.map((teacher, index) => (
           <div className="col-lg-3 col-md-4 col-sm-6 mb-4" key={index}>
